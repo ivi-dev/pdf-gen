437cfca4ff7b3d189bd0912672c10fcfd83bc86e
IliyanVidev <iliyan.videv@industria.tech>
2025-12-31 14:18:08 +0200

    Change shell from "sh" "to bash" in "run.sh" and "test.sh"

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,5 @@
-#!/bin/sh
+#!/bin/bash
 
-java -jar ./target/pdfgen-1.0-SNAPSHOT.jar "$@"
+SCRIPTS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+java -jar "$SCRIPTS_DIR/../target/pdfgen-1.0-SNAPSHOT.jar" "$@"
