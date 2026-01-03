73b2646d125f88e42949a7c1a2b113a87816d494
IliyanVidev <iliyan.videv@industria.tech>
2026-01-03 22:31:30 +0200

    Add Reporters

---
+++ src/main/java/com/pdfgen/reporting/Reporters.java
@@ -0,0 +1,34 @@
+package com.pdfgen.reporting;
+
+import java.util.Map;
+import java.util.function.Consumer;
+
+public class Reporters {
+
+    private Map<String, Reporter> reporters;
+
+    public Reporters(Map<String, Reporter> reporters) {
+        this.reporters = reporters;
+    }
+
+    public Reporter get(String name) {
+        return reporters.get(name);
+    }
+
+    public void invoke(String reporterName, Consumer<Reporter> proc) {
+        proc.accept(reporters.get(reporterName));
+    }
+
+    public void info(String reporterName, String msg, Object ...args) {
+        reporters.get(reporterName).info(msg, args);
+    }
+
+    public void error(String reporterName, String msg, Object ...args) {
+        reporters.get(reporterName).error(msg, args);
+    }
+
+    public void success(String reporterName, String msg, Object ...args) {
+        reporters.get(reporterName).success(msg, args);
+    }
+
+}
